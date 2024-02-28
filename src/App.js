f5e7ea87e02d020638dfa3034d7c41336aae5c56
Aryan Malla <aryanmalla19@gmail.com>
2024-02-28 12:42:21 +0545

    react router installation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Display from './pages/Display';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div>
-      meow meow
+      <Routes>
+        <Route path='/' element={<Display/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
     </div>
   );
 }
